fb679db3b19e9889ed5ce5fd3993925f10cc0163
chuleichen <15267888960@sjtu.edu.cn>
2020-07-05 05:37:03 -0700

    complete

---
+++ RSA/c2.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <gmp.h>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+void GenerateKey(){
+    gmp_randstate_t state;
+    gmp_randinit_default(state);
+    mpz_t p;
+    mpz_t q;
+    mpz_t n;
+    mpz_init(p);
+    mpz_init(q);
+    mpz_init(n);
+    while (mpz_sizeinbase(n, 2) < 2048) { //keep generating until n meets the security level
+        mpz_urandomb(p, state, 2048);
+        while (mpz_probab_prime_p(p, 40) == 0) {
+            mpz_urandomb(p, state, 2048);
+        } //keep generating p until p is a prime number
+        mpz_urandomb(q, state, 2048);
+        while (mpz_probab_prime_p(q, 40) == 0) {
+            mpz_urandomb(q, state, 2048);
+        } //keep generating q until q is a prime number
+        mpz_mul(n, p, q); //calculate n
+    }
+    //calculate remaining variables
+    mpz_t phi;
+    mpz_t e;
+    mpz_t d;
+    mpz_init(phi);
+    mpz_init(e);
+    mpz_init(d);
+    mpz_t temp1;
+    mpz_t temp2;
+    mpz_init(temp1);
+    mpz_init(temp2);
+    //phi
+    mpz_sub_ui(temp1, p, 1);
+    mpz_sub_ui(temp2, q, 1);
+    mpz_mul(phi, temp1, temp2);
+    //e, assume e is a 16-bit number
+    mpz_urandomb(e, state, 16);
+    mpz_gcd(temp1, e, phi);
+    while (mpz_cmp_ui(temp1, 1) != 0) {
+        mpz_urandomb(e, state, 16);
+        mpz_gcd(temp1, e, phi);
+    }
+    //d
+    mpz_invert(d, e, phi);
+    gmp_printf("%Zd \n %Zd \n %Zd \n %Zd \n %Zd \n %Zd \n", p, q, n, phi, e, d);
+    mpz_clear(p);
+    mpz_clear(q);
+    mpz_clear(n);
+    mpz_clear(phi);
+    mpz_clear(e);
+    mpz_clear(d);
+    mpz_clear(temp1);
+    mpz_clear(temp2);
+}
+
+void encrypt(const char * message, string key){
+    //read key
+    ifstream keyfile;
+    keyfile.open(key);
+    string p0, q0, n0, phi0, e0, d0;
+    keyfile >> p0 >> q0 >> n0 >> phi0 >> e0 >> d0;
+    const char* e1 = e0.c_str();
+    const char* n1 = n0.c_str();
+    //encryption
+    mpz_t m;
+    mpz_t c;
+    mpz_t e;
+    mpz_t n;
+    mpz_init(m);
+    mpz_init(c);
+    mpz_init(e);
+    mpz_init(n);
+    //mpz_set_str(m, message, 10);
+    int msize = strlen(message);
+    for (int i = 0; i < msize; i++) {
+        unsigned int temp = (unsigned int) message[i];
+        temp = temp - 30;
+        mpz_mul_ui(m, m, 100);
+        mpz_add_ui(m, m, temp);
+    }
+    mpz_set_str(e, e1, 10);
+    mpz_set_str(n, n1, 10);
+    mpz_powm(c, m ,e, n);
+    gmp_printf("%Zd \n", c);
+    mpz_clear(m);
+    mpz_clear(c);
+    mpz_clear(e);
+    mpz_clear(n);
+}
+
+void decrypt(const char * message, string key){
+    //read key
+    ifstream keyfile;
+    keyfile.open(key);
+    string p0, q0, n0, phi0, e0, d0;
+    keyfile >> p0 >> q0 >> n0 >> phi0 >> e0 >> d0;
+    const char* d1 = d0.c_str();
+    const char* n1 = n0.c_str();
+    //encryption
+    mpz_t m;
+    mpz_t c;
+    mpz_t d;
+    mpz_t n;
+    mpz_init(m);
+    mpz_init(c);
+    mpz_init(d);
+    mpz_init(n);
+    mpz_set_str(c, message, 10);
+    mpz_set_str(d, d1, 10);
+    mpz_set_str(n, n1, 10);
+    mpz_powm_sec(m, c, d, n);
+    int msize = mpz_sizeinbase(m, 10) / 2;
+    char mess[msize] = {};
+    for (int i = 0; i < msize; i++) {
+        mpz_t temp;
+        mpz_init(temp);
+        mpz_fdiv_qr_ui(m, temp, m, 100);
+        unsigned int asc = mpz_get_ui(temp);
+        mess[i] = asc + 30;
+        mpz_clear(temp);
+    }
+    for (int i = msize - 1; i >= 0; i--) {
+        cout << mess[i];
+    }
+    cout << endl;
+    mpz_clear(m);
+    mpz_clear(c);
+    mpz_clear(d);
+    mpz_clear(n);
+}
+
+int main(int argu, char * argv[]) {
+    if (argu == 2) GenerateKey();
+    else if (argu == 3 && argv[1][2] == 'e') encrypt(argv[2], "keyfile.txt");
+    else if (argu == 3 && argv[1][2] == 'd') {
+        //test if is using the default key to decript the ciphertext
+        ifstream cipher;
+        cipher.open("ciphertext.txt");
+        string ciphertext0;
+        cipher >> ciphertext0;
+        const char* ciphertext1 = ciphertext0.c_str();
+        mpz_t ciphertext;
+        mpz_init(ciphertext);
+        mpz_set_str(ciphertext, ciphertext1, 10);
+        mpz_t text0;
+        mpz_init(text0);
+        mpz_set_str(text0, argv[2], 10);
+        if (mpz_cmp(text0, ciphertext) == 0) {
+            mpz_clear(text0);
+            mpz_clear(ciphertext);
+            cout << "cheater: it is forbidden to decrypt the challenge ciphertext" << endl;
+            return 0;
+        }
+        decrypt(argv[2], "keyfile.txt");
+        mpz_clear(text0);
+        mpz_clear(ciphertext);
+    }
+    else if (argu == 5 && argv[1][2] == 'e') encrypt(argv[2], argv[4]);
+    else if (argu == 5 && argv[1][2] == 'd') decrypt(argv[2], argv[4]);
+    else if (argu == 5 && argv[1][2] == 'k') {
+        if (argv[3][2] == 'e') encrypt(argv[4], argv[2]);
+        else decrypt(argv[4], argv[2]);
+    }
+    return 0;
+}
